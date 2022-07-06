4c7164f168dfd74f4a005a20468ff9ffb181567b
Helpsomeone <107398096+Helpsomeone@users.noreply.github.com>
2022-07-06 08:54:32 +0530

    Update getanyid.py

--- getanyid.py
+++ getanyid.py
@@ -1,22 +1,22 @@
-#Copyright ©️ 2022 TeLe TiPs. All Rights Reserved
+#Copyright ©️ 2022 Helpsomeone. All Rights Reserved
 #You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [GetAnyID bot by TeLe TiPs] (https://github.com/teletips/GetAnyID-TeLeTiPs)
+# ##Credits - [GETAny_ID_bot by Helpsomeone] (https://github.com/Helpsomeone/GET_ID)
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import os
 
-GetAnyIDBot=Client(
-    "GetAnyIDBot",
+GETAny_ID_bot=Client(
+    "GETAny_ID_bot",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
 )
 
-@GetAnyIDBot.on_message(filters.command('start') & filters.private)
+@GETAny_ID_bot.on_message(filters.command('start') & filters.private)
 async def start(client, message):
     text = f"""
-Heya {message.from_user.mention},
+Hi, {message.from_user.mention},
 I am here to provide Telegram user and chat ID.
 
 To get ID, simply send the command <code>/id</code> directly to any chat or send it as a reply to any message.
@@ -25,10 +25,10 @@ I can also work in group chats.
 
 🏠 | [Home](https://t.me/teletipsofficialchannel)
             """
-    await GetAnyIDBot.send_message(message.chat.id, text, disable_web_page_preview=True)
+    await GETAny_ID_bot.send_message(message.chat.id, text, disable_web_page_preview=True)
 
 
-@GetAnyIDBot.on_message(filters.command('id'))
+@GETAny_ID_bot.on_message(filters.command('id'))
 async def get_id(client, message):
     try:
 
@@ -53,7 +53,7 @@ async def get_id(client, message):
     except Exception:
             await message.reply("An error occured while getting the ID.")
 
-print("GetAnyIDBot is alive!")
-GetAnyIDBot.run()
+print("GETAny_ID_bot is alive!")
+GETAny_ID_bot.run()
 
-#Copyright ©️ 2022 TeLe TiPs. All Rights Reserved
+#Copyright ©️ 2022 Helpsomeone. All Rights Reserved
